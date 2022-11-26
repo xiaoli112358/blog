9de185ca36df4b7015206f028d5b2bb2b3e063f0
xiaoli112358 <1246468641@qq.com>
2022-11-26 22:23:29 +0800

    把id值进行转化，否则评论表存评论的时候parentID的值是一个损失了精度的值，不是user表原来的id值

--- blogParent/blog-api/src/main/java/com/mszlu/blog/vo/param/CommentParam.java
+++ blogParent/blog-api/src/main/java/com/mszlu/blog/vo/param/CommentParam.java
@@ -1,14 +1,20 @@
 package com.mszlu.blog.vo.param;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
+/**
+ * 添加评论
+ */
 @Data
 public class CommentParam {
-    private Long articleId;
+    @JsonSerialize(using = ToStringSerializer.class)//将查回来的值进行转化，否则会因为长度问题到了前端损失精度
+    private Long articleId;//文章id
 
-    private String content;
+    private String content;//文章内容
 
-    private Long parent;
+    private Long parent;//父评论id
 
-    private Long toUserId;
+    private Long toUserId;//被评论的用户id
 }
