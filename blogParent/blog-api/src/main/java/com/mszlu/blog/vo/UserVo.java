8644b6938c1ba781751ed37ac761c1887898732e
xiaoli112358 <1246468641@qq.com>
2022-11-26 22:40:24 +0800

    uservo也需要给id增加字符串序列化

--- blogParent/blog-api/src/main/java/com/mszlu/blog/vo/UserVo.java
+++ blogParent/blog-api/src/main/java/com/mszlu/blog/vo/UserVo.java
@@ -1,5 +1,7 @@
 package com.mszlu.blog.vo;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import lombok.Data;
 
 @Data
@@ -7,6 +9,6 @@ public class UserVo {
     private String nickname;
 
     private String avatar;
-
+    @JsonSerialize(using = ToStringSerializer.class)//将查回来的值进行转化，否则会因为长度问题到了前端损失精度
     private Long id;
 }
