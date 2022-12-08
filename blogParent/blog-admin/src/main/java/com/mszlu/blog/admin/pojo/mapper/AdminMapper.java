655efbcdd32d6cc5c60099aa031b71c7dd35322e
xiaoli112358 <1246468641@qq.com>
2022-12-08 23:13:13 +0800

    SpringSecurity

---
+++ blogParent/blog-admin/src/main/java/com/mszlu/blog/admin/pojo/mapper/AdminMapper.java
@@ -0,0 +1,7 @@
+package com.mszlu.blog.admin.pojo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mszlu.blog.admin.pojo.Admin;
+
+public interface AdminMapper extends BaseMapper<Admin> {
+}
---
+++ blogParent/blog-admin/src/main/java/com/mszlu/blog/admin/service/AdminService.java
@@ -0,0 +1,33 @@
+package com.mszlu.blog.admin.service;
+
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.mszlu.blog.admin.pojo.Admin;
+import com.mszlu.blog.admin.pojo.Permission;
+import com.mszlu.blog.admin.pojo.mapper.AdminMapper;
+import com.mszlu.blog.admin.pojo.mapper.PermissionMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdminService {
+
+    @Autowired
+    private AdminMapper adminMapper;
+    @Autowired
+    private PermissionMapper permissionMapper;
+
+    public Admin findAdminByUserName(String username){
+        LambdaQueryWrapper<Admin> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(Admin::getUsername,username).last("limit 1");
+        Admin adminUser = adminMapper.selectOne(queryWrapper);
+        return adminUser;
+    }
+
+    public List<Permission> findPermissionsByAdminId(Long adminId){
+        return permissionMapper.findPermissionsByAdminId(adminId);
+    }
+
+}
+
